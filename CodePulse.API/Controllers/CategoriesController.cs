ab44fc2301d7c1b1864ec00f0d20867381c46121
Neto002 <antoniogfneto92@hotmail.com>
2024-04-18 17:35:55 -0300

    first commit

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this._categoryRepository = categoryRepository;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDTO request)
+        {
+            // Map DTO to Domain Model
+
+            Category category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle,
+            };
+
+            await _categoryRepository.CreateAsync(category);
+
+            // Domain model to DTO
+            CategoryDTO response = new CategoryDTO
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle,
+            };
+
+            return Ok(response);
+        }
+
+    }
+}
